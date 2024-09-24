0f6d4ab1f022c9d14d9277cdb77e7198eea7e97a
Ian Wootten <3598161+iwootten@users.noreply.github.com>
2024-09-24 13:35:25 +0100

    Add podcastByGuid

--- tests/test_podcast_lookup.py
+++ tests/test_podcast_lookup.py
@@ -9,6 +9,7 @@ podcastTitle = "This American Life"
 feedUrl = "http://feed.thisamericanlife.org/talpodcast"
 feedId = 522613
 itunesId = 201671138
+guid = "f1ebeaa1-bc5a-534f-8528-0738ae374d55"
 
 
 def test_podcast_lookup_by_feedurl():
@@ -33,3 +34,10 @@ def test_podcast_lookup_by_byitunesid():
 
     results = index.podcastByItunesId(itunesId)
     assert results["feed"]["title"] == podcastTitle, "Did not find the right podcast when doing lookup by Itunes ID"
+
+def test_podcast_lookup_by_byguid():
+    config = podcastindex.get_config_from_env()
+    index = podcastindex.init(config)
+
+    results = index.podcastByGuid(guid)
+    assert results["feed"]["title"] == podcastTitle, "Did not find the right podcast when doing lookup by guid"
